b58a59ffed3def686aee7b9e892c8d80431fc092
Marc Duiker <marcduiker@users.noreply.github.com>
2025-06-22 15:27:23 +0000

    Add Open Sans font and update logo
    Signed-off-by: Marc Duiker <marcduiker@users.noreply.github.com>

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -262,20 +262,9 @@ module.exports = {
     },
     fontFamily: {
       sans: [
+        '"Open Sans"',
         'ui-sans-serif',
         'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        '"Segoe UI"',
-        'Roboto',
-        '"Helvetica Neue"',
-        'Arial',
-        '"Noto Sans"',
-        'sans-serif',
-        '"Apple Color Emoji"',
-        '"Segoe UI Emoji"',
-        '"Segoe UI Symbol"',
-        '"Noto Color Emoji"',
       ],
       serif: ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
       mono: [
